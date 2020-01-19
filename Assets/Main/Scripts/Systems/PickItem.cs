255a6c479b7bf1d93cd4146efe35f8f182447092
Bahadır Üçyıldız <bedo.baho32@yandex.com>
2020-01-20 01:46:02 +0300

    setting up some animations

--- Assets/Main/Scripts/Systems/PickItem.cs
+++ Assets/Main/Scripts/Systems/PickItem.cs
@@ -13,7 +13,6 @@ public class PickItem : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-
         Pick(positionToSpawn, rotationToSpawn);
     }
 
--- Assets/Main/Scripts/Systems/RaycastItemAligner.cs
+++ Assets/Main/Scripts/Systems/RaycastItemAligner.cs
@@ -32,27 +32,16 @@ public class RaycastItemAligner : MonoBehaviour{
 
             Vector3 overlapTestBoxScale = new Vector3(overlapTestBoxSize, overlapTestBoxSize, overlapTestBoxSize);
             Collider[] collidersInsideOverlapBox = new Collider[1];
-            int numberOfCollidersFound = Physics.OverlapBoxNonAlloc(hitInfo.point, overlapTestBoxScale, collidersInsideOverlapBox, spawnRot, spawnedObjectLayer);
-
-            //Debug.Log("number of colliders found " + numberOfCollidersFound);
+            int numberOfCollidersFound = Physics.OverlapBoxNonAlloc(hitInfo.point, overlapTestBoxScale, collidersInsideOverlapBox,
+                                         spawnRot, spawnedObjectLayer);
+                      
 
             if (numberOfCollidersFound == 0)
-            {
-                //Debug.Log("spawned robot");
+            {                
                 _pi.Pick(hitInfo.point, spawnRot);
-            }
-            else
-            {
-                //Debug.Log("name of collider 0 found " + collidersInsideOverlapBox[0].name);
-            }
+            }           
 
         }
-    }
-
-    //void Pick(Vector3 positionToSpawn, Quaternion rotationToSpawn)
-    //{
-    //    int randomIndex = Random.Range(0, itemsToPickFrom.Length);
-    //    GameObject clone = Instantiate(itemsToPickFrom[randomIndex], positionToSpawn, rotationToSpawn);
-    //}
+    }   
 
 }
